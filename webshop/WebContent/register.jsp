eb594992ee5c5e7a26a8dfb64a4389b4a8f9b836
Daniel <DanyL@193.22.75.114>
2017-11-03 12:07:25 +0100

    new register servlet and register.jsp

---
+++ webshop/WebContent/register.jsp
@@ -0,0 +1,75 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@page import="webshop.DBConnector" %>
+<%@page import="java.sql.*"%>
+<%@page import="com.mysql.jdbc.PreparedStatement"%>
+<%@page import="com.mysql.jdbc.Connection"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+  <title>Bucks Money</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <link href="style_home.css" rel="stylesheet">
+  
+</head>
+<body>
+
+  <div class="header">
+  <%@include file="menu_part.jsp" %>
+      <h1>Account erstellen</h1>
+      <form method="post" action="register">
+   		 <input type="text" name="username" value="Name" onclick="this.value=''"/><br/>  
+   		 <input type="text" name="email"  value="Email" onclick="this.value=''"/><br/>  
+   		 <input type="password" name="password"  value="Password" onclick="this.value=''"/><br/>  
+   		 <input type="submit" class="button" value="Registrieren"/>  
+     </form>  
+  </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script>
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+  $(document).ready(function() {
+    $("html").hide().fadeIn(2000);
+  });
+</script>
+</body>
+</html>
---
+++ webshop/src/webshop/register.java
@@ -0,0 +1,118 @@
+package webshop;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class register
+ */
+@WebServlet("/register")
+public class register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		boolean success = false;
+	    String error    = "";
+	    String message  = "";
+	    String name     = request.getParameter("username");
+	    String mail     = request.getParameter("email");
+	    String pass     = request.getParameter("password"); 
+		
+		//Eingabe prüfen
+		if (name == "") {
+			error = "Bitte gib einen Nutzernamen ein.";
+		} else if (pass == "") {
+			error = "Bitte gib ein Passwort ein.";
+		} else if (mail == "") {
+			error = "Bitte gib eine E-Mail Adresse ein.";
+		}
+		
+		try {
+	          Class.forName("com.mysql.jdbc.Driver");
+	          Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/webshop", "root", "");
+	          Statement st=con.createStatement();
+	          
+	          //prüfe ob Username bereits existiert
+	          String sql;
+	      	  sql="SELECT name FROM users WHERE name='"+name+"'";
+	      	  ResultSet rs = null;
+	    	  rs=st.executeQuery(sql);
+	          if (rs.next()) {
+	        	  error = "Ein User mit diesem Namen existiert bereits!";
+	          } 
+	          
+	          //prüfe ob die Mail bereits verwendet wird
+	          sql="SELECT mail FROM users WHERE mail='"+mail+"'";
+	          ResultSet rs2 = null;
+	          rs2=st.executeQuery(sql);
+	       	  if (rs2.next()) {
+	       		  error = "Diese E-Mail wird bereits verwendet.";
+	       	  } 
+	          if (error == "") {
+	       		  MessageDigest alg = MessageDigest.getInstance("MD5");
+	       		  alg.reset();
+	       		  alg.update(pass.getBytes());
+	       		  byte[] digest = alg.digest();
+	       		  StringBuffer hashedpasswd = new StringBuffer();
+	       		  String hx;
+		       	  for (int m=0; m<digest.length; m++){
+		       			hx =  Integer.toHexString(0xFF & digest[m]);
+		       			if(hx.length() == 1){hx = "0" + hx;}
+		       			hashedpasswd.append(hx);
+		       	  }
+		       	  pass = hashedpasswd.toString();
+		       	  int i=st.executeUpdate("insert into users(name,passwort,mail) values('"+name+"','"+pass+"','"+mail+"')");
+	       		  success = true;
+	      	      request.getRequestDispatcher("index.jsp").include(request, response);
+	          }
+	       }
+		
+	       catch(Exception e){
+	       System.out.print(e);
+	       e.printStackTrace();
+	       success = false;
+	       }
+		
+			if (error != "") {
+				message = error;
+	       		request.getRequestDispatcher("error.jsp").include(request, response);  
+			}
+			else if (success == true) {
+				message = "Du hast dich erfolgreich registriert";
+			} else if (success == false) { 
+				message = "Es gab einen Fehler bei der Registrierung. Bitte versuche es später erneut.";
+	       		request.getRequestDispatcher("error.jsp").include(request, response);  
+		    } 
+			
+	}
+
+}
